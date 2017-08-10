ceea785136f33249eb3ea60deb270170e54ade2a
Rafal Grodzinski <rafal@unalignedbyte.com>
2017-08-10 21:52:40 +0100

    Initial Commit

---
+++ Source/Core/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  Square Off AR
+//
+//  Created by Rafal Grodzinski on 10/08/2017.
+//  Copyright © 2017 UnalignedByte. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
