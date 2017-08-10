61df6a0be17bac1d7c34338ea8ae6c3584e8b95a
Rafal Grodzinski <rafal@unalignedbyte.com>
2017-08-10 22:06:30 +0100

    Added Swinject

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '11.0'
+use_frameworks!
+
+target 'Square Off AR' do
+    pod 'Swinject'
+end
---
+++ Pods/Target Support Files/Pods-Square Off AR/Pods-Square Off AR-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Square_Off_AR : NSObject
+@end
+@implementation PodsDummy_Pods_Square_Off_AR
+@end
